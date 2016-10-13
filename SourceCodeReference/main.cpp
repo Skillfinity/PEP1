e5387c2a605afef41692f6773e77de6edd1c5b04
DeepakHebbur <deepak.hebbur@skillfinity.in>
2016-10-13 16:12:12 +0530

    Organized in Folders
    Made changes to organize the files in folder

---
+++ SourceCodeReference/main.cpp
@@ -0,0 +1,31 @@
+/**************************************************************************************************
+                                            FILE HEADER
+              All copy rights reserved by SKILLFINITY TECHNOLOGIES PRIVATE LIMITED - 2016
+
+ File Name: main.cpp
+
+ File Description: main function of the project
+
+ Author: Deepak Ramanna Hebbur
+
+ Date Created: 13/10/2016
+
+ Additional Remarks: Execution starts here!
+
+**************************************************************************************************/
+
+#include <iostream>
+using namespace std;
+#include "employee.h"
+
+int main()
+{
+    Employee main_empEmployee1;
+    main_empEmployee1.Emp_SetEmployeeDetails("Deepak","963001");
+
+    //To test if employee details are properly set, printing the details in console
+    cout << "Employee Name is - "<<main_empEmployee1.Emp_GetEmployeeName() << " and "
+         << "employee Number is - " << main_empEmployee1.Emp_GetEmployeeNumber()<< endl;
+
+    return 0;
+}
