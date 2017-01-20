df82b9f1f0ef3dae2c4aa11d96b1ad0ea4c9ada9
ManasaBhargavan <manasabhargava095@gmail.com>
2017-01-20 10:11:26 +0530

    to display odd numbers

---
+++ SourceCodeReference/ODDEVEN/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,i;
+    printf("Enter the limit x");
+    scanf("%d",&x);
+    for(i=0;i<x;i++)
+    {
+        if((i%2)==0)
+        {
+            continue;
+        }
+        printf("%d \n",i);
+
+    }
+
+    return 0;
+}
