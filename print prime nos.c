e4c922c731aad8977452cc8af56a18bb76eac97b
SUSHMA1996 <sushma.dgowda9085@gmail.com>
2017-01-22 13:22:00 +0530

    Programming Assignment 10

---
+++ print prime nos.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    int x,i,j,isprime;
+    printf("enter the maximum range: x\n");
+    scanf("%d",&x);
+    printf(" the prime numbers between 1 to %d are:\n",x);
+    for(i=2;i<=x;i++)
+    {
+        isprime=0;
+        for(j=2;j<=i/2;j++)
+        {
+            if(i%j==0)
+            {
+                isprime=1;
+                break;
+            }
+        }
+        if(isprime==0 && x!=1)
+            printf("%d\n",i);
+    }
+    return 0;
+}
