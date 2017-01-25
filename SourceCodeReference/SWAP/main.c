37145bdd3e189e0b565eade87ab7604827e5dce0
MinalRao <minal29rao@gmail.com>
2017-01-25 17:12:03 +0530

    PROGRAMMING ASSIGNMENT 2

---
+++ SourceCodeReference/SWAP/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x=10, y=20;
+
+    x=x+y;   //x becomes 30
+    y=x-y;   //y becomes 10
+    x=x-y;   //x becomes 20
+
+    printf("After swapping: x=%d, y=%d \n", x,y);
+    return 0;
+}
+
