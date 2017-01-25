2932a4bcb0c36f40d78b7437b4f67457722676c6
SUSHMA1996 <sushma.dgowda9085@gmail.com>
2017-01-26 00:39:17 +0530

    program assignment 7

---
+++ SourceCodeReference/COMPARE.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int x,y;
+    printf("enter the value of x\n");
+    scanf("%d",&x);
+    printf("enter the value of y\n");
+    scanf("%d",&y);
+   if(x>y)
+    printf("x is greater than y\n");
+   else if(x<y)
+        printf("x is smaller than y\n");
+   else
+         printf("x is equal to y\n");
+    getch();
+	return 0;
+}
