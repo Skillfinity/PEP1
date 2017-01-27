a1d6d6d767c236f7ecbce5d34c95a611e40d9574
mahalakshmisathya <mahalakshmisathya96@gmail.com>
2017-01-26 20:59:38 -0800

    assignment_4

--- SourceCodeReference/main.c
+++ SourceCodeReference/main.c
@@ -3,13 +3,12 @@
 
 int main()
 {
-   int len,wid,area,per;
-   float dia;
-   printf("enter the values of length and width\n");
-   scanf("%d %d",&len,&wid);
-   area=len*wid;
-   per =2*(len+wid);
-   dia=sqrt(len*len+wid*wid);
-   printf("area ,perimeter,diagonal length is: %d,%d,%f\n",area,per,dia);
-   return 0;
+   int age;
+   printf("enter the  age of the person\n");
+   scanf("%d",&age);
+   if(age<18)
+       printf(" not eligible to vote\n");
+    else
+        printf("eligible  to vote\n");
+    return 0;
 }
