bd177589b32c899b37446b75dfaa1bf04df7d431
mahalakshmisathya <mahalakshmisathya96@gmail.com>
2017-01-26 21:49:14 -0800

    assignment_8

--- SourceCodeReference/main.c
+++ SourceCodeReference/main.c
@@ -3,14 +3,35 @@
 
 int main()
 {
- int x,y;
- printf("enter the values\n");
- scanf("%d %d",&x,&y);
- if(x>y)
-    printf("x is greater than y\n");
- else if(x<y)
-    printf("x is lesser than y\n");
- else
-    printf("both are equal\n");
- return 0;
+    char ope;
+    double mul,add,sub,n1,n2,div;
+    printf("enter the operation to performed\n");
+    scanf("%c",&ope);
+    printf("enter the values of n1 and n2\n");
+    scanf("%lf %lf",&n1,&n2);
+
+    switch(ope)
+    {
+    case 'm':
+        mul=n1*n2;
+        printf("mulitiplication output is %0.1lf\n",mul);
+        break;
+    case 'a':
+        add=n1+n2;
+        printf("addition output is: %0.1lf\n",add);
+        break;
+
+    case 's':
+        sub=n1-n2;
+        printf("subtraction output is :%0.1lf\n",sub);
+        break;
+    case 'd':
+        div=n1/n2;
+        printf("division output :is  %0.1lf",div);
+        break;
+    default :
+        printf("invalid");
+
+    }
+    return 0;
 }
