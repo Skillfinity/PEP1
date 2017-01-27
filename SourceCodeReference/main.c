0ac10f7208fef39ca95e8d51fb58b68d269703de
mahalakshmisathya <mahalakshmisathya96@gmail.com>
2017-01-26 22:03:00 -0800

    assignment_9

--- SourceCodeReference/main.c
+++ SourceCodeReference/main.c
@@ -3,35 +3,13 @@
 
 int main()
 {
-    char ope;
-    double mul,add,sub,n1,n2,div;
-    printf("enter the operation to performed\n");
-    scanf("%c",&ope);
-    printf("enter the values of n1 and n2\n");
-    scanf("%lf %lf",&n1,&n2);
-
-    switch(ope)
-    {
-    case 'm':
-        mul=n1*n2;
-        printf("mulitiplication output is %0.1lf\n",mul);
-        break;
-    case 'a':
-        add=n1+n2;
-        printf("addition output is: %0.1lf\n",add);
-        break;
-
-    case 's':
-        sub=n1-n2;
-        printf("subtraction output is :%0.1lf\n",sub);
-        break;
-    case 'd':
-        div=n1/n2;
-        printf("division output :is  %0.1lf",div);
-        break;
-    default :
-        printf("invalid");
-
-    }
-    return 0;
+   int x,i;
+   printf("enter the value of x\n");
+   scanf("%d",&x);
+   for(i=0;i<x;i++)
+   {
+       if(i%2!=0)
+        printf("%d\n",i);
+   }
+   return 0;
 }
