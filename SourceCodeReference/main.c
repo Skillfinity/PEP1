cd559c774bc34b75b19d201d0226a7f7a086b824
mahalakshmisathya <mahalakshmisathya96@gmail.com>
2017-01-26 21:18:54 -0800

    assignment_6

--- SourceCodeReference/main.c
+++ SourceCodeReference/main.c
@@ -3,12 +3,16 @@
 
 int main()
 {
-   int age;
-   printf("enter the  age of the person\n");
-   scanf("%d",&age);
-   if(age<18)
-       printf(" not eligible to vote\n");
+    float percent;
+    printf("enter  your percent:\n");
+    scanf("%f",&percent);
+    if(percent>=85)
+        printf("you are  distinction candidate");
+    else if(percent<85&&percent>=65)
+        printf("you are  first class candidate");
+    else if(percent<65&&percent>=35)
+        printf("you are second class candidate");
     else
-        printf("eligible  to vote\n");
+        printf("candidate got failed ");
     return 0;
 }
